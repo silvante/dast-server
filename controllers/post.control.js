4673f0307c7381ca84fc887715bd623a7c48cf80
silvante <khamidov.ko@gmail.com>
2024-09-04 21:57:35 +0300

    start posts

---
+++ controllers/post.control.js
@@ -0,0 +1,5 @@
+const Post = require("../models/post.model");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../routes/extraRoutes");
+
+module.exports = {};
---
+++ models/post.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 55,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 200,
+  },
+  collection_name: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "collection",
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+});
+
+const Post = mongoose.model("post", postSchema);
+module.exports = Post;
