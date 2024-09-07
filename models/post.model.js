f634f12e6c54e3da46d9049effe72a7ba8b02624
silvante <khamidov.ko@gmail.com>
2024-09-08 01:43:36 +0300

    handling error but not cheking

--- models/post.model.js
+++ models/post.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const Saves = require("./saves.model");
+const Likes = require("./like.model");
 
 const postSchema = mongoose.Schema({
   title: {
@@ -29,6 +31,32 @@ const postSchema = mongoose.Schema({
     type: Date,
     default: Date.now, // Automatically sets the current date when the document is created
   },
+});
+
+postSchema.pre("remove", async function (next) {
+  try {
+    const post = this._id;
+
+    // Delete all saved entries referencing the deleted post
+    await Saves.deleteMany({ post });
+
+    next(); // Continue with the deletion process
+  } catch (error) {
+    next(error); // Handle any errors during the process
+  }
+});
+
+postSchema.pre("remove", async function (next) {
+  try {
+    const post = this._id;
+
+    // Delete all saved entries referencing the deleted post
+    await Likes.deleteMany({ post });
+
+    next(); // Continue with the deletion process
+  } catch (error) {
+    next(error); // Handle any errors during the process
+  }
 });
 
 const Post = mongoose.model("post", postSchema);
