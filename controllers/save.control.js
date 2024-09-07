be43fca2807bd8d5e186d9081b439d32ce495d69
silvante <khamidov.ko@gmail.com>
2024-09-08 01:06:36 +0300

    saves system

--- controllers/save.control.js
+++ controllers/save.control.js
@@ -2,4 +2,79 @@ const Saves = require("../models/saves.model");
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../routes/extraRoutes");
 
-module.exports = {};
+const savePost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      req.status(404).send("login first...");
+    } else {
+      jwt.verify(token, jwtSecret, {}, async (err, userdoc) => {
+        if (err) throw err;
+        try {
+          const saved = await Saves.create({
+            post: id,
+            saver: userdoc.id,
+          });
+          res.status(201).send(saved);
+        } catch (error) {
+          console.log(err);
+          res.send(error);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+};
+
+const unsavePost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const save = await Saves.findById(id);
+    if (!save) {
+      res.send("saved post is not defined");
+    }
+    if (!token) {
+      req.status(404).send("login first...");
+    } else {
+      jwt.verify(token, jwtSecret, {}, async (err, userdoc) => {
+        if (err) throw err;
+        if (userdoc.id !== save.saver) {
+          res.send("server error, sorry for it...");
+        } else {
+          const deleted = await Saves.findByIdAndDelete(save._id);
+          res.status(203).send(deleted);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(err);
+    res.send(error);
+  }
+};
+
+const getMysaves = async (req, res) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      req.status(404).send("login first...");
+    } else {
+      jwt.verify(token, jwtSecret, {}, async (err, userdoc) => {
+        if (err) throw err;
+        const my_saves = await Saves.find({
+          saver: userdoc.id,
+        }).populate("post");
+        res.status(200).send(my_saves);
+      });
+    }
+  } catch (error) {
+    console.log(err);
+    res.send(error);
+  }
+};
+
+module.exports = { savePost, unsavePost, getMysaves };
--- routes/saves.js
+++ routes/saves.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../controllers/save.control");
+const {
+  savePost,
+  unsavePost,
+  getMysaves,
+} = require("../controllers/save.control");
+
+// save post by id
+router.post("/:id", savePost);
+
+// delete save by id
+router.delete("/:id", unsavePost);
+
+// get users saves
+router.get("/", getMysaves);
 
 module.exports = router;
