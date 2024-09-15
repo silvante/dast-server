ff0c68674d7edf6cdd345a6c70dee9aeff683f4c
silvante <khamidov.ko@gmail.com>
2024-09-16 01:05:03 +0300

    equip functions

--- store/inventory.js
+++ store/inventory.js
@@ -38,4 +38,44 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.put("/equip/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      const token = authHeader.split("Bearer ")[1];
+
+      jwt.verify(token, jwtSecret, {}, async (err, userDoc) => {
+        if (err) {
+          return res.status(401).json({ message: "Invalid token" });
+        }
+
+        try {
+          const product = await Product.find({ _id: id });
+          if (!product) {
+            return res.status(404).json({ message: "product not found" });
+          }
+          if (!product.buyers.includes(userDoc.id)) {
+            return res
+              .status(404)
+              .send("you have no this item in your inventory");
+          }
+          const equiped = await User.findByIdAndUpdate(userDoc.id, {
+            check: product._id,
+          });
+          res.status(200).send("your item is equiped");
+        } catch (error) {
+          console.log(error);
+          res.status(500).json({ message: "Server error" });
+        }
+      });
+    } else {
+      res.status(401).json({ message: "No token provided" });
+    }
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+});
+
 module.exports = router;
